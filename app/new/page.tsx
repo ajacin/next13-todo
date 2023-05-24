25edd15efc3960c9d48198f1f9bfe303af414b9d
Arun Jacob <arunjacob2@gmail.com>
2023-05-24 16:01:10 -0400

    changed folder strucure

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -1,4 +1,4 @@
-import { prisma } from "@/src/db";
+import { prisma } from "@/db";
 import { redirect } from "next/navigation";
 import Link from "next/link";
 async function createTodo(data: FormData) {
